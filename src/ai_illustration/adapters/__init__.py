25caea7aa3f5d6410dd525dffab5df33ba6fa16a
shiroku46 <46mastei@gmail.com>
2026-08-06 06:15:35 +0900

    Route ComfyUI execution through quality-aware packaging (#61)

--- src/ai_illustration/adapters/__init__.py
+++ src/ai_illustration/adapters/__init__.py
@@ -2,6 +2,7 @@
 
 from .base import AdapterError, ExecutionPlan, GenerationAdapter
 from .comfyui import ComfyUIAdapter, load_json_object, sanitize_loopback_endpoint, validate_workflow
+from . import comfyui_execution_quality as _comfyui_execution_quality
 from .comfyui_execute import MANIFEST_FILE as COMFYUI_EXECUTION_MANIFEST
 from .comfyui_execute import check_comfyui_execution, prepare_execution, run_comfyui_execution
 
